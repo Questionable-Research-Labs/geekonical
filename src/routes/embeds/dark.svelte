e725e7b67aa42ed3d387984243108023f651e12e
Jasper Miller-Waugh <contact@jmw.nz>
2021-10-19 20:50:29 +1300

    Styles for index and embeds, including custom dark mode

---
+++ src/routes/embeds/dark.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Calendar from "$lib/Calendar.svelte";
+    import "$lib/dark.scss"
+</script>
+<Calendar/>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,4 +7,30 @@ import Calendar from "$lib/Calendar.svelte";
 <h1>
 	Geekonical Calendar
 </h1>
-<Calendar/>
+<div class="calendar-container">
+	<div class="calendar-wrapper">
+		<Calendar/>
+	</div>
+</div>
+<style lang="scss">
+	.calendar-container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		
+		.calendar-wrapper {
+			width: 95vw;
+			@media (min-width: 1000px) {
+				width: 50vw;
+			border-radius: 50px;
+			background: #e0e0e0;
+			box-shadow:  20px 20px 60px #bebebe,
+             -20px -20px 60px #ffffff;
+			 padding: 2em;
+			}
+
+			
+		}
+	}
+</style>
